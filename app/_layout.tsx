cddb703cc98d20528432313552fae34f91c054df
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-03-16 17:38:36 +0200

    feat(layout): simplify stack navigation by consolidating screen options

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,14 +6,7 @@ export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <Stack>
-          <Stack.Screen name="index" options={{ title: "Feed" }} />
-          <Stack.Screen
-            name="notifications"
-            options={{ title: "Notifications", headerShown: false }}
-          />
-          <Stack.Screen name="profile" options={{ title: "Profile" }} />
-        </Stack>
+        <Stack screenOptions={{ headerShown: false }} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
