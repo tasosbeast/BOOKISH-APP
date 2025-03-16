ccd2ca7495104cc75a0554c175effd2f9aaec1ab
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-03-16 17:40:12 +0200

    feat(profile): enhance profile screen layout with styles

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import React from "react";
 
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <Text>profile screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
