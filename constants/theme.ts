3665034728b64961077a7c1d78e355267f25c8d9
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-03-17 21:02:21 +0200

    feat(theme): add sign-in background color and update input opacity

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,5 +1,8 @@
 export const COLORS = {
   "body-bg": "#c1bdba",
   "form-bg": "#13232f",
+  "sign-in-bg": "#1F365C",
+  input: "rgba(10, 11, 11, 0.2)", // Made semi-transparent with 0.7 opacity
+
   main: "#1ab188",
 } as const;
